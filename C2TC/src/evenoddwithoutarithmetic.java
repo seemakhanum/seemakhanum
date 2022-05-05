5f40aafa1dc61d4d1c9a031f443aedeecfc33de1
seemakhanum1 <seemakhanum80t@gmail.com>
2022-05-05 21:12:11 +0530

    my first repository

---
+++ C2TC/src/evenoddwithoutarithmetic.java
@@ -0,0 +1,17 @@
+package C2TC;
+public class evenoddwithoutarithmetic {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int num = 11;	
+        if((num & 1) == 0)
+        {
+        	System.out.println(num +" is even number");
+        }
+        else
+        {
+        	System.out.println(num +" is odd number");	
+        }
+	}
+
+}
